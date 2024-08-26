e1c7f026a27b7464f4d155cb8486e78e7c3c5f62
ediale17 <edithg90@gmail.com>
2024-08-26 01:29:38 +0000

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,20 @@
-console.log('Hello World!')
+console.log("Hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "edith";
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log('will line 11 run?');
+    } else {
+        console.log('will line 13 run?');
+    }
+}
+
+runNow();
