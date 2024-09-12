a1eb7d4059be67662938d43250be320dc04b9cc7
ediale17 <edithg90@gmail.com>
2024-09-12 19:32:19 +0000

    setup and array of objects

--- w5/main.js
+++ w5/main.js
@@ -62,14 +62,14 @@ function determineHouseSizePts(size) {
       const output = document.getElementById("output");
       const newH2 = document.createElement("h2");
       newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
-      // const newH3 = document.createElement("h3");
-      // newH3.textContent = `Based on the number in and size of home`;
-      // const newP = document.createElement("p");
-      // newP.textContent = ` This number is based on the number of people in the house of ${arr[0]} (score: ${arr[3]}).`;
-      // newP.textContent += ` and a ${arr[1]} size of home (score: ${arr[2]}).`;
+      const newH3 = document.createElement("h3");
+      newH3.textContent = `Based on the number in and size of home`;
+      const newP = document.createElement("p");
+      newP.textContent = ` This number is based on the number of people in the house of ${obj.houseM} (score: ${obj.houseMPTS})`;
+      newP.textContent += ` and a(n) ${obj.houseS} size of home (score: ${obj.houseSPTS}).`;
       output.appendChild(newH2);
-      // output.appendChild(newH3);
-      // output.appendChild(newP);
+      output.appendChild(newH3);
+      output.appendChild(newP);
     }
   }
   
