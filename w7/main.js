bdf7e40eee5dd002ee90c97d3a8d4ffcff800739
ediale17 <edithg90@gmail.com>
2024-09-27 04:47:47 +0000

    my attempt at the code, before I review other students code

--- w7/main.js
+++ w7/main.js
@@ -1,42 +1,11 @@
 import { renderTbl } from "./render.js";
+import { determineHouseSizePts, determineHouseHoldPts } from "./cfp.js";
 
 const FORM = document.getElementById("form");
 const OUTPUT = document.getElementById("output");
 const cfpData = [];
 
-function determineHouseSizePts(size) {
-  let houseSizePoints = 0;
-  if (size === "large") {
-    houseSizePoints = 10;
-  } else if (size === "medium") {
-    houseSizePoints = 7;
-  } else if (size === "small") {
-    houseSizePoints = 4;
-  } else if (size === "apt") {
-    houseSizePoints = 2;
-  }
-  return houseSizePoints;
-}
 
-function determineHouseHoldPts(numberInHousehold) {
-  let houseHoldPoints = 0;
-  if (numberInHousehold === 1) {
-    houseHoldPoints = 14;
-  } else if (numberInHousehold === 2) {
-    houseHoldPoints = 12;
-  } else if (numberInHousehold === 3) {
-    houseHoldPoints = 10;
-  } else if (numberInHousehold === 4) {
-    houseHoldPoints = 8;
-  } else if (numberInHousehold === 5) {
-    houseHoldPoints = 6;
-  } else if (numberInHousehold === 6) {
-    houseHoldPoints = 4;
-  } else if (numberInHousehold > 6) {
-    houseHoldPoints = 2;
-  }
-  return houseHoldPoints;
-}
 
 function start(firstName, lastName, houseHoldMembers, houseSize) {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
--- w7/render.js
+++ w7/render.js
@@ -1,4 +1,5 @@
 const TBL = document.getElementById("tab-data");
+let tableRendered = false;
 
 function renderTblHeading() {
     const table = document.createElement("table");
@@ -16,15 +17,25 @@ function renderTblHeading() {
   }
   
   function renderTbl(data) {
+    if(!tableRendered){
     const table = renderTblHeading()
     const tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+    TBL.appendChild(table);
+    tableRendered = true;
+     }
+
+    const table= TBL.querySelector("table");
+    const tbody= table.querySelector("tbody");
     const tr = document.createElement("tr");
-    const trTextArr = ["Edith", 3, "Large", 20]
+    const newSubmit = data[data.length - 1];
+    const trTextArr = [newSubmit.fName, newSubmit.houseM, newSubmit.houseS, newSubmit.cfpTotal]
     trTextArr.forEach (function(text){
       const td =document.createElement("td")
       td.textContent = text;
       tr.appendChild(td);
-    })
+    });
+  
     const td = document.createElement("td")
     const btnEdit = document.createElement("button");
     btnEdit.textContent ="Edit"
@@ -34,8 +45,8 @@ function renderTblHeading() {
     td.appendChild(btnDel);
     tr.appendChild(td);
     tbody.appendChild(tr)
-    table.appendChild(tbody);
-    TBL.appendChild(table);
     }
+
+
 
     export { renderTbl, renderTblHeading};
