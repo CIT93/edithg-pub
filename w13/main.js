9f5ac9acea735c6d7562c9b5af676ae11796dc29
ediale17 <edithg90@gmail.com>
2024-11-08 04:52:47 +0000

    w13 my attempt at the code, before I review other students or solution code.

---
+++ w13/main.js
@@ -0,0 +1,54 @@
+const FORM = document.getElementById("form");
+const TYPE = document.getElementById("exerciseType");
+const REPS = document.getElementById("repNumber");
+const TIME = document.getElementById("timeMinutes");
+const OUTPUT = document.getElementById("output");
+const SUBMIT = document.getElementById("submitError");
+
+const validateField = event => {
+    const field = event.target.value;
+    const fieldId = event.target.id;
+    const fieldError = document.getElementById(`${fieldId}Error`);
+  
+  return new Promise ((resolve, reject) => {
+    if (field === "") {
+      fieldError.textContent = `${fieldId} is required`; //Displays the messsage on the DOM
+      event.target.classList.add("invalid");
+      reject(`${fieldId} is required`);
+    } else {
+      fieldError.textContent = "";
+      event.target.classList.remove("invalid");
+      resolve(`${fieldId} is valid`);
+    }
+  })
+  .catch(fieldError => {
+     console.error(fieldError)
+  })
+};
+  
+  //Attach blur event listeners
+ TYPE.addEventListener("blur", validateField);
+
+  const start = (exerciseType, reps, time) => {
+    OUTPUT.innerHTML = `Start ${exerciseType} for ${time} minutes each rep.`
+
+  setTimeout(()=>{
+    OUTPUT.innerHTML += ` Start!`;
+    setTimeout(()=> {
+      OUTPUT.innerHTML += ` Timer stop.`;
+    }, time*6000)
+  }, 1000)
+};
+
+  FORM.addEventListener("submit", e => {
+    e.preventDefault();
+    if (TYPE.value !== "" && REPS.value !== "" && TIME.value !=="") {
+      SUBMIT.textContent = '';
+      start(TYPE.value, parseInt(REPS.value), parseInt(TIME.value));
+      FORM.reset();
+    } else {
+      SUBMIT.textContent = "Form requires all fields.";
+    }
+  
+  });
+  
